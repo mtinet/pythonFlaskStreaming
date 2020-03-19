ab69ef4d4b8c1530bc67a41bfc0ecd094325bde3
Jason Juhyun Kim <mtinet@hanmail.net>
2020-03-19 23:58:59 +0900

    Update serverTest.py

--- serverTest.py
+++ serverTest.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/hello")
 def hello():
-    return "<h1>JaeSung Fighting</h1>"
+    return "<h1>Jason Fighting</h1>"
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port="8080")
